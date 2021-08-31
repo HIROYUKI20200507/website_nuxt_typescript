b532794a53a0a5ed6c5fe240b47b4240bbbb7036
秋本 裕之 <hiroyuki.akimoto@willof.co.jp>
2021-08-31 20:59:59 +0900

    [bag]v-forでエラー表示になる

--- pages/blog.vue
+++ pages/blog.vue
@@ -3,12 +3,12 @@
     <NubBer />
     <div class="bg-gray-100 py-20 px-20">
       <div class="flex justify-between items-start">
-          <div v-for="qiita in articles" class="bg-white mr-5 max-w-sm" :key="qiita">
+          <div v-for="data of fetchArticles" class="bg-white mr-5 max-w-sm" :key="data.guid">
             <img src="https://images.unsplash.com/photo-1489396160836-2c99c977e970?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60">
             <div class="p-5">
-              <h1 class="text-2xl font-bold text-blue-800 py-2 truncate">{{ qiita.title }}</h1>
-              <p class="bg-white text-sm text-black truncate">{{ qiita.content }}</p>
-              <a :href="qiita.link" class="py-2 mt-4 px-6 text-white bg-blue-500 inline-block rounded">Read More</a>
+              <h1 class="text-2xl font-bold text-blue-800 py-2 truncate">{{ data }}</h1>
+              <p class="bg-white text-sm text-black truncate">{{ data }}</p>
+              <a :href="data.link" class="py-2 mt-4 px-6 text-white bg-blue-500 inline-block rounded">Read More</a>
             </div>
           </div>
       </div>
@@ -25,15 +25,15 @@ export default {
   transition: 'slide-bottom',
   deta(){
     return {
-      articles: '',
+      fetchArticles: '',
     }
   },
   mounted() {
     const self = this;
     axios.get(qiitaURL)
     .then(function(response){
-        console.log(response.data.items)
-        self.qiitaArticles = response.data.items;
+        self.fetchArticles = response.data.items;
+        console.log(self.fetchArticles, 'fetch');
     })
     .catch(function(error){
         console.log(error);
