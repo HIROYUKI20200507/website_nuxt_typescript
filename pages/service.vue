40159fddda93f5eb43c1fce45f87ed01f6d4121b
秋本 裕之 <hiroyuki.akimoto@willof.co.jp>
2021-08-31 15:51:02 +0900

    [modi]serviceページざっくり完成

--- pages/service.vue
+++ pages/service.vue
@@ -1,63 +1,29 @@
 <template>
-    <div>
-        <NubBer />
-        <div class="container flex flex-col px-6 py-4 mx-auto space-y-6 md:h-128 md:py-16 md:flex-row md:items-center md:space-x-6">
-          <div class="flex flex-col items-center w-full md:flex-row md:w-1/2">
-              <div class="flex justify-center order-2 mt-6 md:mt-0 md:space-y-3 md:flex-col">
-                  <button @change="isClickItem(0)" :class="{'bg-blue-500': activeButton[0]}" class="w-3 h-3 mx-2 bg-gray-300 rounded-full md:mx-0 focus:outline-none hover:bg-blue-500"></button>
-                  <button @change="isClickItem(1)" :class="{'bg-blue-500': activeButton[1]}" class="w-3 h-3 mx-2 bg-gray-300 rounded-full md:mx-0 focus:outline-none hover:bg-blue-500"></button>
-                  <button @change="isClickItem(2)" :class="{'bg-blue-500': activeButton[2]}" class="w-3 h-3 mx-2 bg-gray-300 rounded-full md:mx-0 focus:outline-none hover:bg-blue-500"></button>
-                  <button @change="isClickItem(3)" :class="{'bg-blue-500': activeButton[3]}" class="w-3 h-3 mx-2 bg-gray-300 rounded-full md:mx-0 focus:outline-none hover:bg-blue-500"></button>
-              </div>
-              <div class="max-w-lg md:mx-12 md:order-2">
-                  <h1 class="text-3xl font-medium tracking-wide text-gray-800 dark:text-white md:text-4xl">{{ activeConts.title[0] }}</h1>
-                  <p class="mt-4 text-gray-600 dark:text-gray-300">{{ activeConts.title[0] }}</p>
-                  <div class="mt-6">
-                      <a href="#" class="block px-3 py-2 font-semibold text-center text-white transition-colors duration-200 transform bg-blue-500 rounded-md md:inline hover:bg-blue-400">Look up</a>
-                  </div>
-              </div>
+  <div>
+    <NubBer />
+    <div class="container flex flex-col px-6 py-4 mx-auto space-y-6 md:h-128 md:py-16 md:flex-row md:items-center md:space-x-6">
+      <div class="flex flex-col items-center w-full md:flex-row md:w-1/2">
+          <div class="flex justify-center order-2 mt-6 md:mt-0 md:space-y-3 md:flex-col">
+              <button @click="isClickItem(0)" :class="{'bg-blue-500': activeButton[0]}" class="w-3 h-3 mx-2 bg-gray-300 rounded-full md:mx-0 focus:outline-none hover:bg-blue-500"></button>
+              <button @click="isClickItem(1)" :class="{'bg-blue-500': activeButton[1]}" class="w-3 h-3 mx-2 bg-gray-300 rounded-full md:mx-0 focus:outline-none hover:bg-blue-500"></button>
+              <button @click="isClickItem(2)" :class="{'bg-blue-500': activeButton[2]}" class="w-3 h-3 mx-2 bg-gray-300 rounded-full md:mx-0 focus:outline-none hover:bg-blue-500"></button>
+              <button @click="isClickItem(3)" :class="{'bg-blue-500': activeButton[3]}" class="w-3 h-3 mx-2 bg-gray-300 rounded-full md:mx-0 focus:outline-none hover:bg-blue-500"></button>
+              <button @click="isClickItem(4)" :class="{'bg-blue-500': activeButton[3]}" class="w-3 h-3 mx-2 bg-gray-300 rounded-full md:mx-0 focus:outline-none hover:bg-blue-500"></button>
           </div>
-
-          <div class="flex items-center justify-center w-full h-96 md:w-1/2">
-              <img class="object-cover w-full h-full max-w-2xl rounded-md" src="https://images.unsplash.com/photo-1579586337278-3befd40fd17a?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1352&q=80" alt="apple watch photo">
+          <div class="max-w-lg md:mx-12 md:order-2">
+              <h1 class="text-3xl font-medium tracking-wide text-gray-800 dark:text-white md:text-4xl">{{ activeConts.title[0] }}</h1>
+              <p class="mt-4 text-gray-600 dark:text-gray-300">{{ activeConts.item[0] }}</p>
+              <div class="mt-6">
+                  <a target="_blank" :href="activeConts.linkUrl[0]" class="block px-3 py-2 font-semibold text-center text-white transition-colors duration-200 transform bg-blue-500 rounded-md md:inline hover:bg-blue-400">Look up</a>
+              </div>
           </div>
       </div>
 
-        <div class="pb-52">
-            <div class="flex items-center justify-center gap-20 mb-40 perf-top-layout">
-                <img src="~assets/img/Service-img.png" alt="" class="w-2/5 perf-img">
-                <div class="perf-contents">
-                    <a href="#service">
-                        <button class="perf-button">
-                            <div class="pb-4 text-3xl leading-10 contents-main"><span class="layout-radius"></span>Service</div>
-                        </button>
-                    </a>
-                    <p class="flex-wrap w-4/5 contents-sub">
-                        個人・法人関わらず、システム開発・コーポレートサイトやLPの制作などフロントエンドを中心に展開中です。
-                        要件定義からリリース・保守点検まで幅広いサービスの提供をさせていただきております。
-                        制作デザインも承ることが可能です。
-                        <br>
-                        <br>
-                        また、無料でプログラミングを学びたい方も募集しているので、興味がある方は
-                        <span id="service" class="contents-sub-span"><a href="https://form.run/@hiroyuki-akimoto-hp-1615021598">こちら</a></span>
-                        から連絡ください。
-                    </p>
-                    <div class="icon-detail">
-                        <a target="_blank" href="https://github.com/HIROYUKI20200507"><img class="m-3 mt-5 w-7 h-7" src="~assets/img/icon/github-icon.svg" alt="githubアイコン"></a>
-                    </div>
-                </div>
-            </div>
-            <div name="slide">
-                <div class="-mt-20 pref-sub-img" key="img">
-                    <div class="relative mb-20 text-3xl text-center sub-title">Actual results</div>
-                    <li class="flex items-center justify-between gap-5 m-auto mb-40">
-                        <a href="https://isara-mosya.web.app/"><img src="~assets/img/service-sub-img1.png" alt="isara模写"><span>模写コーディング</span></a>
-                        <a href="https://sakata-coach.com/"><img src="~assets/img/service-sub-img2.png" alt="sakataコーチング"><span>コーチングサイト</span></a>
-                    </li>
-                </div>
-            </div>
-        </div>
+      <div class="flex items-center justify-center w-full h-96 md:w-1/2">
+          <img class="object-cover w-full h-full max-w-2xl rounded-md" :src="activeConts.image[0]" alt="image photo">
+      </div>
     </div>
+  </div>
 </template>
 
 <script>
@@ -67,12 +33,40 @@ export default {
     return {
       activeButton: [true, false, false, false],
       activeConts: {
-        title: ['html + css'],
-        item: ['html + css'],
+        title: ['Nuxt.js + Tailwind css + TypeScript'],
+        item: ['現在表示されているサイトです。バックの言語は使っておらず、フロントのNuxt.jsをメインにコーディングしています。'],
+        image: [require('@/assets/img/service-sub-img1.png')],
+        linkUrl: ['/']
       },
       service: {
-        title: ['html + css','html + css + jQuery','Vue.js + axios','React + Redux + ReduxSaga'],
-        item: ['html+css','html+css+jQuery','Vue.js+axios','react+redux+redux saga'],
+        title: [
+          'Nuxt.js + Tailwind css + TypeScript',
+          'html + css + jQuery',
+          'Vue.js + Inertia.js + axios + Laravel Breeze',
+          'React + Redux + ReduxSaga',
+          'html + css + vanilla JS',
+        ],
+        item: [
+          '現在表示されているサイトです。バックの言語は使っておらず、フロントのNuxt.jsをメインにコーディングしています。',
+          'isaraの模写です。基本的なHPの機能を実装しています。',
+          'RSSをJSONで受け取り、Vue.jsで表示を行っております。リアルタイムでニュースとQiita記事の更新をすることが可能です。',
+          'APIのOpen Weather Mapを使い、React+Reduxを使いStore管理を行っています。検索フォームに入力するとリアルタイムでAPIの処理が走ります。',
+          '個人でコーチングをしている方の紹介サイトを作成しました。',
+        ],
+        image: [
+          require('@/assets/img/service-sub-img1.png'),
+          require('@/assets/img/service-sub-img2.png'),
+          require('@/assets/img/service-sub-img3.png'),
+          require('@/assets/img/service-sub-img4.png'),
+          require('@/assets/img/service-sub-img5.png'),
+        ],
+        linkUrl: [
+          '/',
+          'https://isara-mosya.web.app/',
+          'https://github.com/akimoto-willof/laravel_blog',
+          'https://github.com/HIROYUKI20200507/react-test',
+          'https://sakata-coach.com/',
+        ],
       }
     }
   },
@@ -81,13 +75,15 @@ export default {
     setInterval(() => {
       this.activeButton = [false, false, false, false]
       this.activeButton[count++] = true
-      if(count > 3)count = 0
-    }, 4000);
+      if(count > 4)count = 0
+    }, 5000);
   },
   watch: {
     activeButton(i) {
       this.activeConts.title.splice(0, 1, this.service.title[this.activeButton.indexOf(true)])
       this.activeConts.item.splice(0, 1, this.service.item[this.activeButton.indexOf(true)])
+      this.activeConts.image.splice(0, 1, this.service.image[this.activeButton.indexOf(true)])
+      this.activeConts.linkUrl.splice(0, 1, this.service.linkUrl[this.activeButton.indexOf(true)])
     }
   },
   methods: {
@@ -97,6 +93,8 @@ export default {
 
       this.activeConts.title.splice(0, 1, this.service.title[i])
       this.activeConts.item.splice(0, 1, this.service.item[i])
+      this.activeConts.image.splice(0, 1, this.service.image[i])
+      this.activeConts.linkUrl.splice(0, 1, this.service.linkUrl[i])
     }
   }
 
